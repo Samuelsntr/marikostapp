37cf409d2da0f857afd86e93b360e61edd14b99c
Samuelsntr <dodisamuel20@gmail.com>
2022-10-21 15:31:51 +0800

    edit about

---
+++ aboutt.php
@@ -0,0 +1,50 @@
+<?php include "header.php"; ?>
+<!--Image-->
+<div class="container-fluid">
+    <img src="img/10097.jpg" class="img-fluid sampul" alt="">
+</div>
+<div class="container konten">
+    <div class="row mt-5">
+        <div class="col-5">
+            <img src="img/Business team looking for new people.jpg" class="img-thumbnail" alt="...">
+        </div>
+        <div class="col-7 about">
+            <h1>WHAT IS MARIKOST</h1>
+            <p>Platform layanan jasa yang membantu mengatasi masalah pemilik kost, pencari kost bahkan anak kost
+                untuk mengelola, mencari kost hingga memenuhi kebutuhan selama menjadi anak kost. Marikost juga
+                memberikan rekomendasi kost-kostan yang sesuai dengan kategori yang tersedia dan dilengkapi dengan
+                deskripsi yang rinci dan lengkap. Bukan hanya itu saja, Marikost menyediakan berbagai fitur layanan
+                jasa yang dapat membantu pengguna terutama anak kost untuk memenuhi kebutuhan sehari-hari. Gak perlu
+                khawatir dalam menggunakannya karena fitur-fitur yang Marikost sediakan sangat user friendly.
+            </p>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-7 about">
+            <br><br>
+            <h1>MARIKOST GOALS</h1>
+            <p>Tujuan dari Marikost sendiri sederhana yaitu ingin menyediakan suatu platform layanan jasa berbasis
+                teknologi yang mudah digunakan sehingga pengguna seperti pemilik kost, pencari kost hingga anak kost
+                dapat menggunakan satu platform ini untuk mengatasi segala permasalahan yang mereka hadapi.
+            </p>
+        </div>
+        <div class="col-5">
+            <img src="img/marikost.PNG" alt="" class="img-thumbnail">
+        </div>
+    </div>
+    <div class="row mt-5">
+        <div class="col-5">
+            <img src="img/8619-removebg-preview.png" class="img-thumbnail" alt="...">
+        </div>
+        <div class="col-7 about">
+            <h1>OUR UNIQUENESS</h1>
+            <p>Yang membedakan Marikost dengan platform lain salah satunya adalah fitur layanan jasa seperti jasa
+                tukang, jasa laundry, pembayaran, hingga toko kelontong yang di sediakan dalam satu platform.
+                Sehingga, pengguna khususnya pemilik kost dan anak kost tidak perlu khawatir dalam memenuhi
+                kebutuhan sehari-hari selama menjadi anak kost. Marikost siap membantu kalian!
+            </p>
+        </div>
+    </div>
+</div>
+<?php include "footer.php"; ?>
